9d16439c5fdb16f9d5c0b75cbe08591adde5cbe3
Daniel Moreira <danieljlmoreira@gmail.com>
2014-10-20 22:44:39 +0100

    Update app-js

--- server/app.js
+++ server/app.js
@@ -1,105 +1,85 @@
-var restify = require('restify')
+var restify = require('restify'), userSave = require('save')('user'), server = restify.createServer({ name: 'my-api' })
 
-  // Get a persistence engine for the user
-  , userSave = require('save')('user')
-
-  // Create the restify server
-  , server = restify.createServer({ name: 'carryit' })
-
-// Start the server listening on port 3000
 server.listen(3000, function () {
-  console.log('%s listening at %s', server.name, server.url)
+	console.log('%s listening at %s', server.name, server.url)
 })
 
-server
-  // Allow the use of POST
-  .use(restify.fullResponse())
+server .use(restify.fullResponse()) use(restify.bodyParser())
 
-  // Maps req.body to req.params so there is no switching between them
-  .use(restify.bodyParser())
-
-// Get all users in the system
 server.get('/user', function (req, res, next) {
-
-  // Find every entity within the given collection
-  userSave.find({}, function (error, users) {
-
-    // Return all of the users in the system
-    res.send(users)
-  })
+	userSave.find({}, function (error, users) {
+		res.send(users)
+	})
 })
 
-// Get a single user by their user id
 server.get('/user/:id', function (req, res, next) {
-
-  // Find a single user by their id within save
-  userSave.findOne({ _id: req.params.id }, function (error, user) {
-
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-
-    if (user) {
-      // Send the user if no issues
-      res.send(user)
-    } else {
-      // Send 404 header if the user doesn't exist
-      res.send(404)
-    }
-  })
+	userSave.findOne({ _id: req.params.id }, function (error, user) {
+	if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+		if (user) {
+			res.send(user)
+		} else {
+			res.send(404)
+		}
+	})
 })
 
-// Create a new user
 server.post('/user', function (req, res, next) {
-
-  // Make sure name is defined
-  if (req.params.name === undefined && req.params.email === undefined) {
-
-    // If there are any errors, pass them to next in the correct format
-    return next(new restify.InvalidArgumentError('Name must be supplied'))
-  }
-
-  // Create the user using the persistence engine
-  userSave.create({ name: req.params.name, email: req.params.email }, function (error, user) {
-
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-
-    // Send the user if no issues
-    res.send(201, user)
-  })
+	if (req.params.firstName === undefined)
+	{
+		return next(new restify.InvalidArgumentError('First name must be supplied'))
+	}
+	if(req.params.secondName === undefined)
+	{
+		return next(new restify.InvalidArgumentError('Second name must be supplied'))
+	}
+	if(req.params.password === undefined)
+	{
+		return next(new restify.InvalidArgumentError('Password must be supplied'))
+	}
+	if(req.params.email === undefined)
+	{
+		return next(new restify.InvalidArgumentError('Email must be supplied'))
+	}
+	if(req.params.reputation === undefined)
+	{
+		return next(new restify.InvalidArgumentError('Reputation must be supplied'))
+	}
+	if(req.params.phoneNumber === undefined)
+	{
+		return next(new restify.InvalidArgumentError('Phone number must be supplied'))
+	}
+	userSave.create({ firstName: req.params.firstName, secondName: req.params.secondName,
+	   password: req.params.password, email: req.params.email, reputation: req.params.reputation,
+	   phoneNumber: req.params.phoneNumber, }, function (error, user)
+	   {
+			if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
+				res.send(201, user)
+		})
 })
 
-// Update a user by their id
 server.put('/user/:id', function (req, res, next) {
-
-  // Make sure name is defined
-  if (req.params.name === undefined) {
-
-    // If there are any errors, pass them to next in the correct format
-    return next(new restify.InvalidArgumentError('Name must be supplied'))
-  }
-
-  // Update the user with the persistence engine
-  userSave.update({ _id: req.params.id, name: req.params.name }, function (error, user) {
-
-    // If there are any errors, pass them to next in the correct format
-    if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-
-    // Send a 200 OK response
+	if (req.params.password === undefined) 
+	{
+		return next(new restify.InvalidArgumentError('Password must be supplied'))
+	}
+	if(req.params.email === undefined)
+	{
+		return next(new restify.InvalidArgumentError('Email must be supplied'))
+	}
+	if(req.params.phoneNumber === undefined)
+	{
+		return next(new restify.InvalidArgumentError('Phone number must be supplied'))
+	}
+	userSave.update({ _id: req.params.id, email: req.params.email, phoneNumber: req.params.phoneNumber, password: req.params.password }, function (error, user)
+	{
+		if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
     res.send(200)
   })
 })
 
-// Delete user with the given id
 server.del('/user/:id', function (req, res, next) {
-
-  // Delete the user with the persistence engine
   userSave.delete(req.params.id, function (error, user) {
-
-    // If there are any errors, pass them to next in the correct format
     if (error) return next(new restify.InvalidArgumentError(JSON.stringify(error.errors)))
-
-    // Send a 200 OK response
     res.send()
   })
 })
