8d5dc49c85c94994e6761b7f0cad779a7ff2b977
ricardoneves93 <ricardofilipe65@gmail.com>
2014-12-22 22:39:15 +0000

    edit message almost done

--- client/www/js/modules/message/controllers.js
+++ client/www/js/modules/message/controllers.js
@@ -2,5 +2,9 @@ var module = angular.module('messageModule');
 
 module.controller('messageCtrl', function($scope, $ionicPopup, $state, makeRequest, BACache) {
 
-
+	$scope.updateEditor = function() {
+	    var element = document.getElementById("message-body");
+	    console.log(element.style.height);
+	    element.style.height = element.scrollHeight + "px";
+	};
 });
