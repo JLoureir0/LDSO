ea0be803d79657bbad01a1747e2ca04b272cbf01
Daniel Moreira <danieljlmoreira@gmail.com>
2015-01-01 15:19:17 +0000

    Refacotring

--- client/www/js/modules/bootstrap/controllers.js
+++ client/www/js/modules/bootstrap/controllers.js
@@ -20,164 +20,114 @@ module.controller('toogleCtrl', function($scope, $ionicSideMenuDelegate, $state,
 		maxDesv: '10'
 	};
 
-	/*Temporary trips*/
-	/*
-	$scope.trips = [
-	{startPoint: 'Paranhos',
-		destPoint: 'Lisboa',
-		weekDay: 'Qua.',
-		monthDay: '20',
-		month: 'Set',
-		startTime: {hour: '19', minute: '30'},
-		scheduleEndTime: {hour: '23', minute: '15'},
-		objectTypes: ['frágil', 'inflamável'],
-		minPrice: '5',
-		maxDesv: '20'
-	}, 
-	{startPoint: 'Porto',
-		destPoint: 'Aveiro',
-		weekDay: 'Qua.',
-		monthDay: '20',
-		month: 'Set',
-		startTime: {hour: '19', minute: '30'},
-		scheduleEndTime: {hour: '23', minute: '15'},
-		objectTypes: ['frágil', 'inflamável'],
-		minPrice: '5',
-		maxDesv: '20'
-	},
-	{startPoint: 'Trofa',
-		destPoint: 'Rio Tinto',
-		weekDay: 'Qua.',
-		monthDay: '20',
-		month: 'Set',
-		startTime: {hour: '19', minute: '30'},
-		scheduleEndTime: {hour: '23', minute: '15'},
-		objectTypes: ['frágil', 'inflamável'],
-		minPrice: '5',
-		maxDesv: '20'
-	},
-	{startPoint: 'Algarve',
-		destPoint: 'Bragança',
-		weekDay: 'Qua.',
-		monthDay: '20',
-		month: 'Set',
-		startTime: {hour: '19', minute: '30'},
-		scheduleEndTime: {hour: '23', minute: '15'},
-		objectTypes: ['frágil', 'inflamável'],
-		minPrice: '5',
-		maxDesv: '20'
+	$scope.showRegisterFooter = false;
+	$scope.showLoginFooter = false;
+
+	$scope.toggleLeft = function() {
+		$ionicSideMenuDelegate.toggleLeft();
 	}
-];*/
 
-$scope.showRegisterFooter = false;
-$scope.showLoginFooter = false;
+	$scope.selectItem = function($index) {
 
-$scope.toggleLeft = function() {
-	$ionicSideMenuDelegate.toggleLeft();
-}
-
-$scope.selectItem = function($index) {
-
-	var currentHash = BACache.get('session');
-	if(currentHash) {
-		switch($index) {
-			case 0:
-				$state.go('menu.my-trips');
-				break;
-			case 1:
-				$state.go('menu.search-trip');
-				break;
-			case 2:
-				$state.go('menu.share-trip');
-				break;
-			case 3:
-				$state.go('menu.messages');
-				break;
-			case 4:
-				$state.go('menu.login');
-				logOut();
-				break;
-			case 5:
-				showDialog();
-				break;
-			default:
-				break;
-		}
-	} else {
+		var currentHash = BACache.get('session');
+		if(currentHash) {
 			switch($index) {
-			case 0:
-				$state.go('menu.search-trip');
-				break;
-			case 1:
-				showDialog();
-				break;
-			default:
-				break;
+				case 0:
+					$state.go('menu.my-trips');
+					break;
+				case 1:
+					$state.go('menu.search-trip');
+					break;
+				case 2:
+					$state.go('menu.share-trip');
+					break;
+				case 3:
+					$state.go('menu.messages');
+					break;
+				case 4:
+					$state.go('menu.login');
+					logOut();
+					break;
+				case 5:
+					showDialog();
+					break;
+				default:
+					break;
+			}
+		} else {
+				switch($index) {
+				case 0:
+					$state.go('menu.search-trip');
+					break;
+				case 1:
+					showDialog();
+					break;
+				default:
+					break;
+			}
 		}
 	}
-}
 
-$scope.updateFootersAndSidebar = function() {
+	$scope.updateFootersAndSidebar = function() {
 
-	// deal with footers
-	if($state.current.url === '/login')
-		$scope.showRegisterFooter = true;
-	else
-		$scope.showRegisterFooter = false;
+		// deal with footers
+		if($state.current.url === '/login')
+			$scope.showRegisterFooter = true;
+		else
+			$scope.showRegisterFooter = false;
 
-	if($state.current.url === '/registo')
-		$scope.showLoginFooter = true;
-	else 
-		$scope.showLoginFooter = false;
+		if($state.current.url === '/registo')
+			$scope.showLoginFooter = true;
+		else 
+			$scope.showLoginFooter = false;
 
-	// deal with side menu
-    var alreadySetUsername = makeRequest.getUserName();
-	if(typeof alreadySetUsername !== 'undefined') {
-		$scope.username = alreadySetUsername;
+		// deal with side menu
+	    var alreadySetUsername = makeRequest.getUserName();
+		if(typeof alreadySetUsername !== 'undefined') {
+			$scope.username = alreadySetUsername;
+		}
+		else {
+			$scope.username = "";
+		}
+
+		var userhash = BACache.get('session');
+		if(typeof username !== 'undefined') {
+			document.getElementById("menu_username").style.display = 'inline';
+			$scope.items = [
+				{item: 'As tuas viagens'},
+				{item: 'Procurar viagens'},
+				{item: 'Partilhar viagem'},
+				{item: 'Mensagens'},
+				{item: 'Terminar sessão'},
+				{item: 'Sair'}
+			];
+		} else {
+			document.getElementById("menu_username").style.display = 'none';
+			$scope.items = [
+				{item: 'Procurar viagens'},
+				{item: 'Sair'}
+			];
+		}
 	}
-	else {
-		$scope.username = "";
+
+	$scope.checkIfLoggedIn = function() {
+		if(BACache.get('session') != null)
+			$state.go('menu.profile');
 	}
 
-	var userhash = BACache.get('session');
-	if(typeof username !== 'undefined') {
-		document.getElementById("menu_username").style.display = 'inline';
-		$scope.items = [
-			{item: 'As tuas viagens'},
-			{item: 'Procurar viagens'},
-			{item: 'Partilhar viagem'},
-			{item: 'Mensagens'},
-			{item: 'Terminar sessão'},
-			{item: 'Sair'}
-		];
-	} else {
-		document.getElementById("menu_username").style.display = 'none';
-		$scope.items = [
-			{item: 'Procurar viagens'},
-			{item: 'Sair'}
-		];
+	function logOut() {
+		BACache.removeAll();
+		makeRequest.resetUserVariables();
+		$scope.updateFootersAndSidebar();
+		$state.go('login');
 	}
-}
-
-$scope.checkIfLoggedIn = function() {
-	if(BACache.get('session') != null)
-		$state.go('menu.profile');
-}
-
-function logOut() {
-	BACache.removeAll();
-	makeRequest.resetUserVariables();
-	$scope.updateFootersAndSidebar();
-	$state.go('login');
-}
 
 
 /*********Close the application***********/
 
   //Function that will show popup asking if user wants to exit application or not
   function showDialog() {
-  	navigator.notification.beep(1);
-  	navigator.notification.confirm("Deseja sair da aplicação?", exitApplication, "Sair", "Sim, Não");
+  	navigator.notification.confirm("Deseja sair da aplicação?", exitApplication, "Sair", "Não, Sim");
   }
 
   // If the user clicks yes the application will close
